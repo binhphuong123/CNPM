2969a7e9157f1589458c61d43a709dfa20ba500c
binhphuong123 <binhpromax123@gmail.com>
2026-01-25 09:34:05 +0700

    Delete test_function directory

--- test_function/test_connectMySQL.py
+++
@@ -1,9 +0,0 @@
-from connectdb.connectMySQL import connect_mysql
-
-conn = connect_mysql()
-
-if conn:
-    print("Kết nối MySQL thành công")
-    conn.close()
-else:
-    print(" Kết nối MySQL thất bại")
